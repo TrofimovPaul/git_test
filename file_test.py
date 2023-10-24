29b539d18e98be10c8e5769290b3e7f4dbea05ed
TrofimovPaul <trofimovpv95@mail.ru>
2023-10-24 21:23:30 +0300

    first commit

---
+++ file_test.py
@@ -0,0 +1,26 @@
+def calculator(a=None, action=None, b=None):
+    if a is None or b is None or action is None:
+        a = int(input('Enter first number: '))
+        action = input('Choose + or - or * or / ')
+        b = int(input('Enter second number: '))
+    if action == '+':
+        return print('result:',a+b)
+    elif action == '-':
+        return print('result:',a-b)
+    elif action == '*':
+        return print('result:',a*b)
+    elif action == '/':
+        if b != 0:
+            return print('result:',a/b)
+        else:
+            return print('Warning! b = 0. Division is impossible')
+
+calculator(3,'+',0)
+print('\n')
+calculator(3,'-',0)
+print('\n')
+calculator(3,'*',0)
+print('\n')
+calculator(3,'/',0)
+print('\n')
+calculator()
